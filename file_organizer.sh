76f00801e1130481873ef5c937a0d87ab06c06ee
mahmoud-hamdy66 <mahmoudhamdy.a60@gmail.com>
2025-03-16 12:14:18 +0200

    update

--- file_organizer.sh
+++ file_organizer.sh
@@ -2,20 +2,20 @@
 #the source dir 
 source_dir="/path/to/your/source/folder"
 #destinationdir 
-pdf_dir="path/to/pdf/folder"
-doc_dir="path/to/doc/folder"
-img_dir="path/to/img/folder"
+pdf_dir="/path/to/pdf/folder"
+doc_dir="/path/to/doc/folder"
+png_dir="/path/to/img/folder"
 
 #mkdir for destination if it is not exist
 
-mkdir -p  "pdf_dir"
-mkdir -p "doc_dir"
-mkdir -p "img_dir"
+mkdir -p  "$pdf_dir"
+mkdir -p  "$doc_dir"
+mkdir -p  "$png_dir"
 
 #move files 
 
 mv "$source_dir"/*.pdf "$pdf_dir" 2> /dev/null
-mv "$source_dir"/*.img "$img_dir" 2> /dev/null
+mv "$source_dir"/*.png "$png_dir" 2> /dev/null
 mv "$source_dir"/*.doc "$doc_dir" 2> /dev/null
 
 echo " done "
